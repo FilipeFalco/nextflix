e9087a98e6c26ff78fe910eef04706fc3a2cddfd
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2020-12-14 19:55:50 -0400

    Fianlização da API route do /user

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,5 +1,31 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
+import connect from "../../utils/database";
 
-export default (req, res) => {
-    res.status(200).json({text: "API Funcionando"})
-}
+export default async (req, res) => {
+
+  // Detecção do método que esta sendo utilizado
+  // Permitir somente POST
+  // Só conecta se for o método correto { POST }
+  if (req.method === "POST") {
+    const { name, email, date_birth} = req.body;
+
+    // Caso a requisição não possua a variavel { name } correta, mostra mensagem de erro e não realiza o registro
+    if(!name || !email || !date_birth) {
+        res.status(400).json({ error: 'Missing body parameter' });
+        return;
+    }
+    
+    const { db } = await connect();
+
+    const response = await db.collection("user").insertOne({
+      name, 
+      email, 
+      date_birth
+    });
+
+    res.status(200).json(response.ops[0]);
+  } else {
+      // Caso não seja o método correto, exibe erro
+    res.status(400).json({ error: "Wrong request method" })
+  }
+};
--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,10 @@ const client = new MongoClient(process.env.DATABASE_URL, {
     useUnifiedTopology: true, 
 });
 
-async function connect() {
-    await client.connect();
+export default async function connect() {
+    // Caso não haja uma conexão com o banco de dados já existente, então, croia uma nova conexão
+    if(!client.isConnected()) await client.connect();
+
+    const db = client.db("projeto-jera");
+    return { db, client };
 }
