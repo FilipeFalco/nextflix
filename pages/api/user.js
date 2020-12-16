cd871f643f9dc8a3971774ca389476ac2cad9da2
Filipe Ferreira Falco <falcoefilipe@hotmail.com>
2020-12-15 23:17:53 -0400

    Finalizado API Route de criação de usuário

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,31 +1,67 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import connect from "../../utils/database";
 
 export default async (req, res) => {
-
-  // Detecção do método que esta sendo utilizado
-  // Permitir somente POST
-  // Só conecta se for o método correto { POST }
+  /**
+  *  Método POST 
+  **/ 
   if (req.method === "POST") {
-    const { name, email, date_birth} = req.body;
+    const { name, email, date_birth, whitelist } = req.body;
 
-    // Caso a requisição não possua a variavel { name } correta, mostra mensagem de erro e não realiza o registro
-    if(!name || !email || !date_birth) {
-        res.status(400).json({ error: 'Missing body parameter' });
-        return;
+    // Caso a requisição não possua a variável { name } correta, mostra mensagem de erro e não realiza o registro
+
+    if (!name || !email || !date_birth) {
+      res.status(400).json({ error: "Missing body parameter" });
+      return;
     }
-    
+
+    // Conecta no Banco de Dados
     const { db } = await connect();
+    
+    // Busca pelo e-mail
+    const emailAlreadyRegistered = await db
+      .collection('users')
+      .findOne({ email })
 
-    const response = await db.collection("user").insertOne({
-      name, 
-      email, 
-      date_birth
-    });
+    // Valida se o e-mail já esta registrado
+    if(emailAlreadyRegistered) {
+      const response = await db.collection("user").insertOne({
+        name,
+        email,
+        date_birth,
+        // É um array de objetos
+        whitelist: whitelist || [],
+      });
 
-    res.status(200).json(response.ops[0]);
-  } else {
-      // Caso não seja o método correto, exibe erro
-    res.status(400).json({ error: "Wrong request method" })
+      res.status(200).json(response.ops[0]);
+    } else {
+      res.status(400).json({error: "E-mail already exist"})
+    }
+      
+    /**
+     *  Método GET 
+     **/ 
+    } else if (req.method === "GET") {
+      const { email } = req.body;
+
+      // Caso email não exista
+      if (!email) {
+        res.status(400).json({ error: "Missing user e-mail on request body" });
+        return;
+      }
+
+      const { db } = await connect();
+
+      const response = await db
+        .collection("user")
+        .findOne({ email });
+
+      if (!response) {
+        res.status(400).json({ error: "User not found" });
+        return;
+      }
+      res.status(200).json(response);
+    } else {
+    // Caso não seja o método correto, exibe erro
+    res.status(400).json({ error: "Wrong request method" });
   }
 };
---
+++ utils/moviedb.js
@@ -0,0 +1,3 @@
+import { MovieDb } from ('moviedb-promise')
+
+const moviedb =  new MovieDb('process.env.MOVIEDB_KEY')
